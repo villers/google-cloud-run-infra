c0f76b0429fb8c3fc0896991aff37dbe5c7ac9ed
VILLERS Mickaël <mickael.villers@epitech.eu>
2022-01-27 22:48:03 +0100

    refactor deployment with terraform

--- infrastructure/terraform/_modules/network/main.tf
+++ infrastructure/terraform/_modules/network/main.tf
@@ -15,6 +15,12 @@ resource "google_compute_subnetwork" "cloudrun" {
   region                   = var.region
   network                  = google_compute_network.self.id
   private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.1
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
 resource "google_vpc_access_connector" "cloudrun" {
